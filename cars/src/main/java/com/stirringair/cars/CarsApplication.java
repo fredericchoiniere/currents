8639058d39e27874ae20df18680d47fc95476df3
PherdTheBird <fredericchoiniere1@gmail.com>
2022-07-09 16:45:02 -0400

    test

--- cars/src/main/java/com/stirringair/cars/CarsApplication.java
+++ cars/src/main/java/com/stirringair/cars/CarsApplication.java
@@ -15,8 +15,8 @@ public class CarsApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String hello(@RequestParam(value = "test", defaultValue = "World") String name) {
+		return String.format("Hello %s!    %s", name, name);
 	}
 
 	@GetMapping("/test")
